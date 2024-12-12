4d1db9cf4ed0c4fae26d98464c4775111fcd6ae9
penxy <srctywf@outlook.com>
2024-12-12 13:51:26 +0800

    添加release文件夹

---
+++ release/Linux/etc/client.js
@@ -0,0 +1,8 @@
+{
+    user="pxy",
+    password="16759",
+    ip_ftp="192.168.64.132",
+    key="adsfadsfasmfasfsafhcasmfkasjfasf",
+    iv="adsfasmfasfsadsfasmfasfsafhcafhc",
+    db="release/Linux/etc/main.db"
+}
